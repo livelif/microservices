863060710d3e1880220988427c30364681dfd37c
Paulo Bernardo <paulo.ianes.bernardo@gmail.com>
2021-01-10 14:34:10 -0300

    Configuring hr-user project

--- hr-user/src/test/java/com/paulo/hruser/HrUserApplicationTests.java
+++ hr-user/src/test/java/com/paulo/hruser/HrUserApplicationTests.java
@@ -2,7 +2,9 @@ package com.paulo.hruser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootTest
 class HrUserApplicationTests {
 
