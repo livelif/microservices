513115ebba6921ae686f45a8f2e74a61f38d4ffd
Paulo Bernardo <paulo.ianes.bernardo@gmail.com>
2021-01-10 17:58:26 -0300

    UserRepository, UserResource, Zuul config

--- hr-user/src/main/java/com/paulo/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/paulo/hruser/HrUserApplication.java
@@ -1,19 +1,17 @@
 package com.paulo.hruser;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication implements CommandLineRunner {
 
 	
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+	//@Autowired
+	//private BCryptPasswordEncoder passwordEncoder;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
