2cdae96692a41679d58ff0bed03466769dbf3300
Paulo Bernardo <paulo.ianes.bernardo@gmail.com>
2021-01-10 17:08:10 -0300

    Role, user, many to many associations

--- hr-user/src/test/java/com/paulo/hruser/HrUserApplicationTests.java
+++ hr-user/src/test/java/com/paulo/hruser/HrUserApplicationTests.java
@@ -2,9 +2,8 @@ package com.paulo.hruser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaClient
+
 @SpringBootTest
 class HrUserApplicationTests {
 
